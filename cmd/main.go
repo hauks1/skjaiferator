d54cb23f78b3b4159c2478c95e9d9c1f457a17f8
hauks1 <hauk.storjord@bekk.no>
2025-10-08 14:48:40 +0200

    🚧 Created a new v1beta1 API version
    Used the operators-sdk to create a v1beta1 API version. This is step two
    from the turorial.The command used
    operator-sdk create api --group skjaif --version v1beta1 --kind
    SvartSkjaif --resource

--- cmd/main.go
+++ cmd/main.go
@@ -38,6 +38,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
 
 	skjaifv1alpha1 "github.com/hauks1/skjaiferator/api/v1alpha1"
+	skjaifv1beta1 "github.com/hauks1/skjaiferator/api/v1beta1"
 	"github.com/hauks1/skjaiferator/internal/controller"
 	// +kubebuilder:scaffold:imports
 )
@@ -51,6 +52,7 @@ func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
 	utilruntime.Must(skjaifv1alpha1.AddToScheme(scheme))
+	utilruntime.Must(skjaifv1beta1.AddToScheme(scheme))
 	// +kubebuilder:scaffold:scheme
 }
 
